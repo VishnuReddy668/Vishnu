b5924f26bce2c00e87628ab44ecc5d6e177ae2f3
Vishnu Vardhan Reddy.Palakolanu <vishnureddy6686@gmail.com>
2024-07-20 12:59:39 +0530

    Create scripts.js

---
+++ Task1/scripts.js
@@ -0,0 +1,6 @@
+### 3. JavaScript (scripts.js)
+javascript
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+});
